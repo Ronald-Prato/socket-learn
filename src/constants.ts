5872d0636381bc14005e6fb4de4cde8ed0c10f1d
Ronald Prato <ronald.prato@unet.edu.ve>
2021-04-23 01:08:49 -0500

    frontend pointing to production instances

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const SOLOQ_URI = 'http://localhost:8000'
-export const GAME_URI = 'http://localhost:8003'
+export const SOLOQ_URI = 'http://34.218.237.18:8000'
+export const GAME_URI = 'http://54.188.56.122:8003'
