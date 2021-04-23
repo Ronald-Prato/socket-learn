75a945e2001531886d5a06585707c07f77e90d74
Ronald Prato <ronald.prato@unet.edu.ve>
2021-04-23 01:18:30 -0500

    endpoints placed under https

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const SOLOQ_URI = 'http://34.218.237.18:8000/'
-export const GAME_URI = 'http://54.188.56.122:8003/'
+export const SOLOQ_URI = 'https://34.218.237.18:8000'
+export const GAME_URI = 'https://54.188.56.122:8003'
